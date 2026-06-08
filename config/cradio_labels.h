17c5f2d73e54cd2b1b9fa8c30556e19dfec7dc4d
Jared Ning <jared@redningja.com>
2026-06-08 15:27:46 -0500

    Label keys.

---
+++ config/cradio_labels.h
@@ -0,0 +1,45 @@
+#pragma once
+
+// Left Hand Alpha Rows (Pinky to Index)
+#define LT4 0
+#define LT3 1
+#define LT2 2
+#define LT1 3
+#define LT0 4
+
+#define LM4 10
+#define LM3 11
+#define LM2 12
+#define LM1 13
+#define LM0 14
+
+#define LB4 20
+#define LB3 21
+#define LB2 22
+#define LB1 23
+#define LB0 24
+
+// Right Hand Alpha Rows (Index to Pinky)
+#define RT0 5
+#define RT1 6
+#define RT2 7
+#define RT3 8
+#define RT4 9
+
+#define RM0 15
+#define RM1 16
+#define RM2 17
+#define RM3 18
+#define RM4 19
+
+#define RB0 25
+#define RB1 26
+#define RB2 27
+#define RB3 28
+#define RB4 29
+
+// Thumbs (LH1=Outer, LH0=Inner | RH0=Inner, RH1=Outer)
+#define LH1 30
+#define LH0 31
+#define RH0 32
+#define RH1 33
